ebf786c78c3b44a61fa91d8d9f5d8414b2811c20
Adem Ben Amor <54805093+dissojak@users.noreply.github.com>
2026-05-04 17:08:39 +0100

    remove all logs in prod

--- next.config.ts
+++ next.config.ts
@@ -3,27 +3,18 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "res.cloudinary.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "images.pexels.com",
-        port: "",
-        pathname: "/**",
-      },
+      { protocol: "https", hostname: "res.cloudinary.com", port: "", pathname: "/**" },
+      { protocol: "https", hostname: "images.unsplash.com", port: "", pathname: "/**" },
+      { protocol: "https", hostname: "images.pexels.com", port: "", pathname: "/**" },
     ],
   },
-  // devIndicators: false, 
+
+  compiler: {
+    // Removes console.log/info/debug in production builds (keeps console.error)
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+
+  // devIndicators: false,
 };
 
 export default nextConfig;
